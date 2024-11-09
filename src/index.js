50eed6d0f7777431fe6421e6516597c2b6faf5e6
XxAngelo3040xX <116958065+XxAngelo3040xX@users.noreply.github.com>
2024-11-09 12:30:53 +0100

    callback

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import { createForm } from "./scripts/form.js";
 import { createMap } from "./scripts/mappe.js";
+import { generateFetchComponent } from "./scripts/fetchCache.js";
 
 const form = createForm(document.querySelector('#app'));
-  form.setLabels(["Luogo"]);
-  form.onsubmit(console.log);
-  form.render();
-
 const map = createMap(document.getElementById("map"));
-map.render();
+const fetchComp = generateFetchComponent();
+
+form.setLabels(["Luogo"]);
+form.render();
+
+fetchComp.build("../../config.json").then(()=>{
+    form.onsubmit((value) => {
+        fetchComp.getData(value).then((data)=>{
+            map.addPlace(value, data);
+            map.render();
+        }).catch(console.error);
+    });
+}).catch(console.error);
